2c3356e26428fc4a569961be7cf6c11157c7e09a
PanjiSri <neiabaraja04@gmail.com>
2024-09-05 22:55:32 +0700

    fix: reparasi ANN dan reparasi penjelasan ANN, semoga aman

--- src/supervised-learning/ann-scratch.py
+++ src/supervised-learning/ann-scratch.py
@@ -1,11 +1,18 @@
 import numpy as np
 
 class Layer:
-    def __init__(self, input_size, output_size, activation):
+    def __init__(self, input_size, output_size, activation, init_method='he'):
         self.input_size = input_size
         self.output_size = output_size
         self.activation = activation
-        self.weights = np.random.randn(input_size, output_size) * np.sqrt(2. / input_size)
+        
+        if init_method == 'he':
+            self.weights = np.random.randn(input_size, output_size) * np.sqrt(2. / input_size)
+        elif init_method == 'xavier':
+            self.weights = np.random.randn(input_size, output_size) * np.sqrt(1. / input_size)
+        else:
+            raise ValueError("Metode inisialisasi tidak dikenal. Gunakan 'he' atau 'xavier'.")
+
         self.biases = np.zeros((1, output_size))
         self.input = None
         self.output = None
@@ -16,7 +23,7 @@ class Layer:
         self.output = self.apply_activation(self.z)
         return self.output
 
-    def backward(self, output_gradient, learning_rate, batch_size):
+    def backward(self, output_gradient, learning_rate, batch_size, regularization=None, reg_lambda=0.01):
         if self.activation == 'softmax':
             activation_gradient = output_gradient
         else:
@@ -25,12 +32,14 @@ class Layer:
         input_gradient = np.dot(activation_gradient, self.weights.T)
         weights_gradient = np.dot(self.input.T, activation_gradient) / batch_size
         biases_gradient = np.sum(activation_gradient, axis=0, keepdims=True) / batch_size
-        
-        if self.regularization == 'l1':
-            weights_gradient += self.reg_lambda * np.sign(self.weights) / batch_size
-        elif self.regularization == 'l2':
-            weights_gradient += self.reg_lambda * self.weights / batch_size
 
+        # Menambahkan regularisasi pada weights_gradient
+        if regularization == 'l1':
+            weights_gradient += reg_lambda * np.sign(self.weights) / batch_size
+        elif regularization == 'l2':
+            weights_gradient += reg_lambda * self.weights / batch_size
+
+        # Update weights dan biases
         self.weights -= learning_rate * weights_gradient
         self.biases -= learning_rate * biases_gradient
         
@@ -61,19 +70,18 @@ class Layer:
             raise ValueError(f"Fungsi aktivasi tidak didukung: {self.activation}")
 
 class ANN_Scratch:
-    def __init__(self, layer_sizes, activations, loss='mse', regularization=None, reg_lambda=0.01, learning_rate=0.01, epochs=1000, batch_size=32):
+    def __init__(self, layer_sizes, activations, loss='mse', regularization=None, reg_lambda=0.01, learning_rate=0.01, epochs=1000, batch_size=32, init_method='he'):
         self.layers = []
         self.regularization = regularization
         self.reg_lambda = reg_lambda
-        for i in range(len(layer_sizes) - 1):
-            layer = Layer(layer_sizes[i], layer_sizes[i+1], activations[i])
-            layer.regularization = self.regularization
-            layer.reg_lambda = self.reg_lambda
-            self.layers.append(layer)
         self.loss = loss
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.batch_size = batch_size
+
+        for i in range(len(layer_sizes) - 1):
+            layer = Layer(layer_sizes[i], layer_sizes[i+1], activations[i], init_method)
+            self.layers.append(layer)
 
     def forward(self, X):
         for layer in self.layers:
@@ -85,7 +93,7 @@ class ANN_Scratch:
         gradient = self.loss_derivative(y, output)
         
         for layer in reversed(self.layers):
-            gradient = layer.backward(gradient, self.learning_rate, self.batch_size)
+            gradient = layer.backward(gradient, self.learning_rate, self.batch_size, self.regularization, self.reg_lambda)
 
     def fit(self, X, y):
         for epoch in range(self.epochs):
@@ -94,9 +102,9 @@ class ANN_Scratch:
                 y_batch = y[i:i+self.batch_size]
                 self.backward(X_batch, y_batch)
             
-            if epoch % 1 == 0: 
-                loss = self.calculate_loss(X, y)
-                print(f"Epoch {epoch+1}, Loss: {loss}")  
+            # if epoch % 10 == 0: 
+            loss = self.calculate_loss(X, y)
+            print(f"Epoch {epoch}, Loss: {loss}")  
 
     def predict(self, X):
         return self.forward(X)
